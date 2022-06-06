29a166a1740fb72dc8c99235d5e6246ec3a85b4e
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-06-06 07:24:42 -0700

    Complete this source code in order to print the integer stored in the variable number, followed by Battery street

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 98
-# convert strings into integer
-	number = int(number)
-# then print Battery street
-	print(f"{98} Battery street")
+	>>> print(f"{98} Battery street")
